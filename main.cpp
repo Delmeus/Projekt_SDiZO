c9d6d480ec9313ee86a8b7d19cbf2bc82725d07d
Delmeus <93483813+Delmeus@users.noreply.github.com>
2023-04-09 22:10:23 +0200

    Added time measurement for searching for elements

--- main.cpp
+++ main.cpp
@@ -52,7 +52,7 @@ int main() {
                 break;
             } //ręczny test BST
             case 5: {
-                long results[6] = {0, 0, 0,0, 0, 0};
+                long results[7] = {0, 0, 0,0, 0, 0, 0};
                 std::cout << "\nBeginning automatic table test\n";
                 for(int i = 0; i < 15; i++) {
                     std::cout << "\nTest nr: " << i + 1;
@@ -66,13 +66,14 @@ int main() {
                 std::cout << "Removing  first  element: " << results[3]/15000000 << " ms " << results[3]/15000 << " microseconds " << results[3]/15 << " ns\n";
                 std::cout << "Removing  given  element: " << results[4]/15000000 << " ms " << results[4]/15000 << " microseconds " << results[4]/15 << " ns\n";
                 std::cout << "Removing  last   element: " << results[5]/15000000 << " ms " << results[5]/15000 << " microseconds " << results[5]/15 << " ns\n";
+                std::cout << "Searching for    element: " << results[6]/15000000 << " ms " << results[6]/15000 << " microseconds " << results[6]/15 << " ns\n";
                 std::cout << "\n\nEnd of test, press ENTER to continue..." << std::endl;
                 std::cin.ignore(10000, '\n');
                 getchar();
                 break;
             } //automatyczny test tablicy
             case 6: {
-                long results[6] = {0, 0, 0,0, 0, 0};
+                long results[7] = {0, 0, 0,0, 0, 0,0};
                 std::cout << "\nBeginning automatic list test\n";
                 for(int i = 0; i < 15; i++) {
                     std::cout << "\nTest nr: " << i + 1;
@@ -86,6 +87,7 @@ int main() {
                 std::cout << "Removing  first  element: " << results[3]/15000000 << " ms " << results[3]/15000 << " microseconds " << results[3]/15 << " ns\n";
                 std::cout << "Removing  given  element: " << results[4]/15000000 << " ms " << results[4]/15000 << " microseconds " << results[4]/15 << " ns\n";
                 std::cout << "Removing  last   element: " << results[5]/15000000 << " ms " << results[5]/15000 << " microseconds " << results[5]/15 << " ns\n";
+                std::cout << "Searching for    element: " << results[6]/15000000 << " ms " << results[6]/15000 << " microseconds " << results[6]/15 << " ns\n";
                 std::cout << "\n\nEnd of test, press ENTER to continue..." << std::endl;
                 std::cin.ignore(10000, '\n');
                 getchar();
@@ -93,15 +95,16 @@ int main() {
             } //automatyczny test listy
             case 7: {
                 std::cout << "\nBeginning automatic heap test\n";
-                long results[2] = {0, 0};
+                long results[3] = {0, 0,0};
                 for(int i = 0; i < 15; i++) {
                     std::cout << "\nTest nr: " << i + 1;
                     FileRandomizer::knownSizeWrite(size);
                     AutomaticTester::testHeap(results, i + 1);
                 }
                 std::cout << "\nHeap test average results" << std::endl;
-                std::cout << "Adding   element: " << results[0]/15000000 << " ms " << results[0]/15000 << " microseconds " << results[0]/15 << " ns\n";
-                std::cout << "Removing element: " << results[1]/15000000 << " ms " << results[1]/15000 << " microseconds " << results[1]/15 << " ns\n";
+                std::cout << "Adding        element: " << results[0]/15000000 << " ms " << results[0]/15000 << " microseconds " << results[0]/15 << " ns\n";
+                std::cout << "Removing      element: " << results[1]/15000000 << " ms " << results[1]/15000 << " microseconds " << results[1]/15 << " ns\n";
+                std::cout << "Searching for element: " << results[2]/15000000 << " ms " << results[2]/15000 << " microseconds " << results[2]/15 << " ns\n";
                 std::cout << "\n\nEnd of test, press ENTER to continue..." << std::endl;
                 std::cin.ignore(10000, '\n');
                 getchar();
